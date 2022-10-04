dd7cac0ba5d4cb3a37a90bcc7ebef8a79afc5705
Amogh Singh <singh.amogh@gmail.com>
2022-10-04 18:11:28 -0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "AMOGH SINGH";
+const meta = {
+    title: "Amogh Singh",
+    description: "I’m Amogh Singh. I love to code and enjoy spending time in solving coding problems",
+};
+
+const introdata = {
+    title: "I’m Amogh Singh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "",
+    your_img_url: "images/Display_picture.jpg",
+};   
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "Passionate Computer Science Grad student with an aptitude for learning and exploring new technologies. Strong knowledge of Python Programming Language with the ability to successfully treat large sized Data. Hands-on experience of implementing projects on cleaning, visualization of data and building data apps. Quick learner and a great team player who can also work well independently.",
+};
+const educationtimeline = [{
+    institution: "Clemson University",
+    degree: "Master's in Computer Science",
+    date: "Jan 2021 - Dec 2021",
+},
+{
+    institution: "University of Mumbai",
+    degree: "Bachelor's in Computer Engg",
+    date: "Aug 2015 - May 2019",
+},
+];
+const worktimeline = [{
+        jobtitle: "Graduate Teaching Assistant",
+        where: "Clemson University",
+        date: "June 2021 - December 2021",
+    },
+    {
+        jobtitle: "Data Science Intern",
+        where: "AirrChip Inc",
+        date: "June 2020 - November 2020",
+    },
+    {
+        jobtitle: "Database Development & Analysis Intern",
+        where: "Furus Packaging Pvt Ltd",
+        date: "June 2018 - December 2018",
+    },
+];
+
+const skills = [{
+        name: "Python-Scripting (Python/BeautifulSoup/NumPy/Pandas)",
+        value: 90,
+    },
+    {
+        name: "Database Management (MySQL/MongoDB/PostgreSQL)",
+        value: 85,
+    },
+    {
+        name: "Frontend Javascript Frameworks (React, Bootstrap, Angular )",
+        value: 80,
+    },
+    {
+        name: "Backend Javascript Frameworks (Node, Express)",
+        value: 80,
+    },
+    {
+        name: "Cloud (AWS, GCP, Docker)",
+        value: 85,
+    },
+    {
+        name: "Softwares (Visual Studio, Jupyter, Spyder )",
+        value: 80,
+    },
+    
+];
+
+const services = [{
+        title: "AWS Certified: Cloud Practitioner",
+        description: "Issued by AMAZON WEB SERVICES Training and Certification.",
+    },
+    {
+        title: "Data Science Foundations",
+        description: "Issued by Neebal Initiative.",
+    },
+    {
+        title: "Python For Data Science, AI & Development",
+        description: "Issued by Coursera.",
+    },
+];
+
+const dataportfolio = [{
+        img: "images/Russel_pic.PNG",
+        desctiption: "NBA Player Salary Prediction",
+        link: "https://github.com/amoghsingh1297/Salary-Prediction-NBA-Dataset",
+    },
+    {
+        img: "images/login.png",
+        desctiption: "MeTube System- A Youtube Clone",
+        link: "https://github.com/clarson98/MeTube",
+    },
+    {
+        img: "images/movies.PNG",
+        desctiption: "Movie Recommender System",
+        link: "https://github.com/amoghsingh1297/Movie-Recommender",
+    },
+    {
+        img: "images/taxi.jpg",
+        desctiption: "Taxi Cab Fare Prediction",
+        link: "https://github.com/amoghsingh1297/Taxi-Cab-Fares-",
+    },
+    {
+        img: "images/gym_mze.PNG",
+        desctiption: "Solving Gym-Maze scenario using RL techniques",
+        link: "#",
+    },
+    {
+        img: "images/Clemson.PNG",
+        desctiption: "Clemson Data Analysis",
+        link: "https://github.com/amoghsingh1297/Clemson-Data_Analysis",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "amoghss@clemson.edu",
+    YOUR_PHONE: "(864)324-3962",
+    description: "Let me hear from you if you like my work. Send me a text to plan meetups to discuss innovative ideas with a cup of coffee..",
+    // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_dtwju1r",
+    YOUR_TEMPLATE_ID: "template_hlkhj9g",
+    YOUR_USER_ID: "XlCbSpZ9weDPhKLXs",
+};
+
+const socialprofils = {
+    github: "https://github.com/amoghsingh1297/",
+    facebook: "https://facebook.com/amogh.singh12",
+    linkedin: "https://linkedin.com/amoghsingh/",
+    twitter: "https://twitter.com/AmoghSi29391105",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    educationtimeline,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
